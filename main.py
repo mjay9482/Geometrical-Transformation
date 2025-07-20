251964735f62a37cdb56f4811687261a860d646a
mjay9482 <mr98719@gmail.com>
2025-07-21 02:21:02 +0530

    added velocity profiling

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
-import numpy
+import numpy as np
 from utils import *
 from plot_transformation import Animation
 
 class SE3Pose: 
-    def __init__(self, pos, rot_mat = None):
+    def __init__(self, pos, rot_mat=None, velocity=None):
         self.pos = np.array(pos)
         self.rot_mat = rot_mat if rot_mat is not None else np.eye(3)
+        self.velocity = np.array(velocity) if velocity is not None else np.zeros(3)
 
     def __repr__(self):
-        return f"Pose={self.pos} Orientation={self.rot_mat}"
+        return f"Pose={self.pos} Orientation={self.rot_mat} Velocity={self.velocity}"
     
     def interpolate(self, next, alpha):
-        interploated_pos = (1-alpha) * self.pos + alpha * next.pos 
+        interpolated_pos = (1 - alpha) * self.pos + alpha * next.pos 
         q0, q1 = rotation_matrix_to_quaternion(self.rot_mat), rotation_matrix_to_quaternion(next.rot_mat) 
-        interpolated_q = slerp(q0,q1,alpha) 
+        interpolated_q = slerp(q0, q1, alpha) 
         interpolated_r = quaternion_to_rotation_matrix(interpolated_q)
-        return SE3Pose(interploated_pos, interpolated_r)
+        return SE3Pose(interpolated_pos, interpolated_r)
+
+def velocity_profile(n):
+    t = np.linspace(-6, 6, n)  
+    sigmoid = 1 / (1 + np.exp(-t))
+    return (sigmoid - sigmoid.min()) / (sigmoid.max() - sigmoid.min())  
 
 def main():
-    start = np.array([-1,-1,1])
-    rot_i = np.array([[1,-0.4,0.1],
-                    [0,0.6,1],
-                    [0.2,1,0]])
-    goal = np.array([3,3,3])
-    rot_g = np.array([[1,0,-1],
-                    [-0.5,1,1],
-                    [0,-1,0.5]])
+    start = np.array([-1, -1, 1])
+    rot_i = np.array([[1, -0.4, 0.1],
+                      [0, 0.6, 1],
+                      [0.2, 1, 0]])
+    goal = np.array([3, 3, 3])
+    rot_g = np.array([[1, 0, -1],
+                      [-0.5, 1, 1],
+                      [0, -1, 0.5]])
+
     poseA = SE3Pose(start, rot_i)
     poseB = SE3Pose(goal, rot_g)
 
-    in_between_pos = [poseA.interpolate(poseB,alpha) for alpha in np.linspace(0,1,100)]
+    n = 100
+    alphas = velocity_profile(n)  
+    in_between_pos = [poseA.interpolate(poseB, alpha) for alpha in alphas]
+
+    dt = 1.0 / n
+
+    for i in range(1, n - 1):
+        v = (in_between_pos[i + 1].pos - in_between_pos[i - 1].pos) / (2 * dt)
+        in_between_pos[i].velocity = v
+
+    in_between_pos[0].velocity = (in_between_pos[1].pos - in_between_pos[0].pos) / dt
+    in_between_pos[-1].velocity = (in_between_pos[-1].pos - in_between_pos[-2].pos) / dt
+
+    for pose in in_between_pos:
+        pose.speed = np.linalg.norm(pose.velocity)
 
     anim = Animation(in_between_pos)
     anim.animate()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- plot_transformation.py
+++ plot_transformation.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 import os
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
+from matplotlib import cm
 
 class Animation: 
     
@@ -13,6 +16,7 @@ class Animation:
         self.ax0 = self.fig.add_subplot(111, projection='3d')
         self.quivers = []
         self.setup_plot()
+        self.draw_speed_path() 
         
     def setup_plot(self):
         self.ax0.set_facecolor('black')
@@ -27,7 +31,7 @@ class Animation:
         self.ax0.yaxis.label.set_color('white')
         self.ax0.zaxis.label.set_color('white')
         self.ax0.title.set_color('white')
-        self.ax0.set_title("SE3 Pose Transformation")
+        self.ax0.set_title("SE3 Pose Transformation + Velocity")
 
     def update_plot(self, i):
         for quiv in self.quivers:
@@ -36,24 +40,45 @@ class Animation:
 
         pos = self.poses[i].pos
         rot = self.poses[i].rot_mat
+        vel = self.poses[i].velocity
+
         length = 2.0
-        
         origin = pos
         x_axis = rot[:, 0] * length
         y_axis = rot[:, 1] * length
         z_axis = rot[:, 2] * length
 
-        self.quivers.append(
-            self.ax0.quiver(*origin, *x_axis, color='r', linewidth=2)
-        )
-        self.quivers.append(
-            self.ax0.quiver(*origin, *y_axis, color='g', linewidth=2)
-        )
-        self.quivers.append(
-            self.ax0.quiver(*origin, *z_axis, color='b', linewidth=2)
-        )
-        
+        self.quivers.append(self.ax0.quiver(*origin, *x_axis, color='r', linewidth=2))
+        self.quivers.append(self.ax0.quiver(*origin, *y_axis, color='g', linewidth=2))
+        self.quivers.append(self.ax0.quiver(*origin, *z_axis, color='b', linewidth=2))
+
+        # Add velocity vector (magenta)
+        self.quivers.append(self.ax0.quiver(*origin, *vel, color='m', linewidth=1.5))
+
         return self.quivers
+
+    def draw_speed_path(self):
+        positions = np.array([pose.pos for pose in self.poses])
+        speeds = np.array([pose.speed for pose in self.poses])
+
+        norm_speeds = (speeds - speeds.min()) / (speeds.max() - speeds.min() + 1e-6)
+        cmap = cm.get_cmap("jet")  # Or "plasma", "viridis", etc.
+
+        for i in range(len(positions) - 1):
+            p1 = positions[i]
+            p2 = positions[i + 1]
+            seg = np.vstack((p1, p2)).T
+            color = cmap(norm_speeds[i])
+            self.ax0.plot(*seg, color=color, linewidth=2)
+            
+        norm = Normalize(vmin=speeds.min(), vmax=speeds.max())
+        sm = ScalarMappable(norm=norm, cmap=cmap)
+        sm.set_array([])
+        cbar = self.fig.colorbar(sm, ax=self.ax0, shrink=0.5, pad=0.1)
+        cbar.set_label("Speed Magnitude", color='white')
+        cbar.ax.yaxis.set_tick_params(color='white')
+        plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'), color='white')
+
 
     def animate(self, save=True):
         ani = animation.FuncAnimation(
